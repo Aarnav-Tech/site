6c0c1f834ce0d63ad4ffee55c34cfe484ead633f
Aarnav Lokesh <106572373+Aarnav-Tech@users.noreply.github.com>
2025-01-25 13:53:16 +0530

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,142 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Aarnav.',
+  tagline: 'Help.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://aarnav.rf.gd',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Aarnav-Tech', // Usually your GitHub org/user name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+                  },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+        blog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Aarnav Lokesh',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Home',
+          },
+
+          {
+            type: 'docSidebar',
+            sidebarId: 'notesSidebar',
+            position: 'left',
+            label: 'Notes - Class 10',
+          },
+
+          {
+            href: 'https://github.com/Aarnav-Tech',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Home',
+                to: '/',
+              },
+              {
+                label: 'Notes - Class 10',
+                to: '/notes',
+              },
+            ],
+          },
+          {
+            title: 'Community (WIP)',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: '#',
+              },
+              {
+                label: 'Discord',
+                href: '#',
+              },
+              {
+                label: 'X',
+                href: '#',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Aarnav-Tech',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Aarnav Lokesh.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
